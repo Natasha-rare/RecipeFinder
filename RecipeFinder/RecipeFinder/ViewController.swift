3a713ae1991aa3ac3714841162468cdb4d80f1e9
Natasha-rare <Natasha_ea5@mail.ru>
2020-07-09 10:45:54 +0300

    Added title and button START (without action)

--- RecipeFinder/RecipeFinder/ViewController.swift
+++ RecipeFinder/RecipeFinder/ViewController.swift
@@ -14,8 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.frame = CGRect(x: 50, y: 50, width: 300, height: 40)
+        label.textColor = UIColor.black
+        label.text = "Recipe Finder"
+        label.font = UIFont(name: "MarkerFelt-Thin", size: 45)
+        
+        
+        let buttonStart = UIButton()
+        buttonStart.setTitle("START", for: .normal)
+        buttonStart.setTitleColor(UIColor.blue, for: .normal)
+        buttonStart.frame = CGRect(x: 15, y: -50, width: 300, height: 500)
+        buttonStart.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
+        
+        super.view.addSubview(label)
+        super.view.addSubview(buttonStart)
+        
     }
-
-
+    
+    @objc func buttonClicked(sender : UIButton) {
+        
+        print("Button Clicked")
+    }
+    
 }
+
 
