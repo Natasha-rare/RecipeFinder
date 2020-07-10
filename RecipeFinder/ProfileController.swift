64f482fcb26a541fb6e0f462ca6e59eafb4f87b3
u.irene <janelake2017@gmail.com>
2020-07-10 15:37:25 +0300

    теперь label в profile по центру
    теперь label в profile по центру

--- RecipeFinder/ProfileController.swift
+++ RecipeFinder/ProfileController.swift
@@ -11,10 +11,9 @@ import UIKit
 class ProfileController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .white
         let label = UILabel()
-        label.frame = CGRect(x: 3, y: 85, width: 300, height: 53)
+        label.frame = CGRect(x: 3, y: 85, width: 370, height: 53)
         label.textColor = UIColor.black
         label.text = "Your profile"
         label.font = UIFont(name: "MarkerFelt-Wide", size: 40)
