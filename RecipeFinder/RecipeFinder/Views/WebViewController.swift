f46a78cd5d74a813924b8216c6b3f625db840139
u.irene <janelake2017@gmail.com>
2020-07-17 21:04:57 +0300

    delete my changes

--- RecipeFinder/RecipeFinder/Views/WebViewController.swift
+++ RecipeFinder/RecipeFinder/Views/WebViewController.swift
@@ -1,11 +1,3 @@
-//
-//  WebViewController.swift
-//  RecipeFinder
-//
-//  Created by Daniel Khromov on 7/17/20.
-//  Copyright © 2020 Наталья Автухович. All rights reserved.
-//
-
 import Foundation
 import UIKit
 import WebKit
@@ -13,11 +5,9 @@ import WebKit
 class WebViewController: UIViewController, WKUIDelegate{
     
     var url = ""
-    var button = NeoButton()
     override func viewDidLoad() {
-        button.load(title: "done", frame: CGRect(x: 20, y: 20, width: 150, height: 58))
+    
         super.viewDidLoad()
-        super.view.addSubview(button)
         setupUI()
         print("URL_URL_URL\(url)")
         let myRequest = URLRequest(url: URL(string: url)!)
