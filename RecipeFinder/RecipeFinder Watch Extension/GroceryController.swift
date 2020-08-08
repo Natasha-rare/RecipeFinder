9eb294dff260c24f706dbd8755dcc21d248964b4
u.irene <janelake2017@gmail.com>
2020-08-08 21:44:25 +0300

    watch app

--- RecipeFinder/RecipeFinder Watch Extension/GroceryController.swift
+++ RecipeFinder/RecipeFinder Watch Extension/GroceryController.swift
@@ -23,14 +23,30 @@ public struct User: Codable {
 class GroceryController: WKInterfaceController {
     @IBOutlet weak var GroceryTable: WKInterfaceTable!
     var ingredients: String = ""
-    var ingr: [Substring] = []
+    var ingr: [String] = []
+    var groceryIngr: [Substring] = []
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
-        fetchIngredients()
-        print(ingredients)
-        
+//        fetchIngredients()
+        if UserDefaults.standard.bool(forKey: "logged") == true{
+            ingredients = UserDefaults.standard.string(forKey: "productList")!
+            groceryIngr = ingredients.split(separator: "|")
+            for item in groceryIngr {
+                ingr.append(String(item))
+            }
+            GroceryTable.setNumberOfRows(self.ingr.count, withRowType: "Row")
+            var string: [String] = []
+            for item in self.ingr {
+                string.append(String(item))
+            }
+            for (index, product) in string.enumerated(){
+                guard let row = self.GroceryTable.rowController(at: index) as? GroceryRow else {continue}
+                row.label.setText(product)
+            }
+            print(ingredients)
+        }
     }
 
     override func willActivate() {
@@ -42,39 +58,43 @@ class GroceryController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-    func fetchIngredients(){
-        print("into")
-        var email = UserDefaults.standard.string(forKey: "email") ?? "Nope"
-        print(email)
-        var password = UserDefaults.standard.string(forKey: "password") ?? "Nope"
-        print(password)
-        email = "irene@gmail.com"
-        password = "9823010176adff1ed75beb313e8fdd61a66b526355df05fc90d7f683af8ec024"
-        let url =  "https://recipe-finder-api-nodejs.herokuapp.com/?email=\(email)&password=\(password)"
-        print(url)
-        AF.request(url, method: .get).response {
-        response in
-        if let data = response.value{
-            let json = try? JSONSerialization.jsonObject(with: data!, options: [])
-                
-            if let dict = json as? [String: Any]{
-                print("DICTIONARY:", dict)
-                var grocery = dict["productList"] as! String
-                print("GROCERY:", grocery)
-                self.ingredients = grocery
-                self.ingr = self.ingredients.split(separator: "|")
-                print("ingr:",self.ingr)
-                self.GroceryTable.setNumberOfRows(self.ingr.count, withRowType: "Row")
-                var string: [String] = []
-                for item in self.ingr {
-                    string.append(String(item))
-                }
-                for (index, product) in string.enumerated(){
-                    guard let row = self.GroceryTable.rowController(at: index) as? GroceryRow else {continue}
-                    row.label.setText(product)
-                }
-            }
-        }
-        }
-    }
+//    func fetchIngredients(){
+//        print("into")
+//        var email = UserDefaults.standard.string(forKey: "email") ?? "Nope"
+//        print(email)
+//        var password = UserDefaults.standard.string(forKey: "password") ?? "Nope"
+//        print(password)
+//        email = "irene@gmail.com"
+//        password = "9823010176adff1ed75beb313e8fdd61a66b526355df05fc90d7f683af8ec024"
+//        let url =  "https://recipe-finder-api-nodejs.herokuapp.com/?email=janelake2017@gmail.com&password=ff551d515feb96cef76a2d2a9b98f4ee1895bca58a7c86c00a6249827442f13c"
+//        print(url)
+//        AF.request(url, method: .get).response {
+//        response in
+//        if let data = response.value{
+//            let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+//
+//            if let dict = json as? [String: Any]{
+//                print("into")
+//                print("DICTIONARY:", dict)
+//                let grocery = dict["productList"] as! String
+//                print("GROCERY:", grocery)
+//                self.ingredients = grocery
+//                self.ingr = self.ingredients.split(separator: "|")
+//                print("ingr:",self.ingr)
+//                self.ingr = ["banana", "apple", "raspberry"]
+//                print("INGR:", self.ingr)
+//                print("INGR:", self.ingr.count)
+//                self.GroceryTable.setNumberOfRows(self.ingr.count, withRowType: "Row")
+//                var string: [String] = []
+//                for item in self.ingr {
+//                    string.append(String(item))
+//                }
+//                for (index, product) in string.enumerated(){
+//                    guard let row = self.GroceryTable.rowController(at: index) as? GroceryRow else {continue}
+//                    row.label.setText(product)
+//                }
+//            }
+//        }
+//        }
+//    }
 }
--- RecipeFinder/RecipeFinder Watch Extension/InterfaceController.swift
+++ RecipeFinder/RecipeFinder Watch Extension/InterfaceController.swift
@@ -8,13 +8,66 @@
 
 import WatchKit
 import Foundation
-
+import CryptoKit
+import Alamofire
 
 class InterfaceController: WKInterfaceController {
-
+    var Password : String = ""
+    var Email : String = ""
+    @IBAction func emailText(_ value: NSString?) {
+        self.Email = value! as String
+        print("E:", Email)
+    }
+    @IBAction func passwordText(_ value: NSString?) {
+        self.Password = value! as String
+        print("P:", Password)
+    }
+    @IBOutlet weak var email: WKInterfaceTextField!
+    @IBOutlet weak var password: WKInterfaceTextField!
+    @IBAction func buttonDonePressed() {
+        Password = "123Password"
+        Email = "janelake2017@gmail.com"
+        let hash = "\(self.Password).\(self.Email)"
+        let hashedPassword1 = SHA256.hash(data: Data(hash.utf8))
+        let hashedPassword = hashedPassword1.map { String(format: "%02hhx", $0) }.joined()
+        print("HASHED:", hashedPassword.description)
+        UserDefaults.standard.set(false, forKey: "logged")
+        print("HELLO:", UserDefaults.standard.bool(forKey: "logged"))
+        let url = "https://recipe-finder-api-nodejs.herokuapp.com/?email=\(self.Email)&password=\(String(hashedPassword.description))"
+        print(url)
+        auth(email: Email, password: String(hashedPassword.description)) {
+        result in
+            print("heyheyhey")
+            print("!", String(result))
+        if result == "Logged in!"{
+            let url = "https://recipe-finder-api-nodejs.herokuapp.com/?email=\(self.Email)&password=\(String(hashedPassword.description))"
+            print(url)
+            AF.request(url, method: .get).response{
+                response in
+                if let data = response.value{
+                    print("next")
+                    let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+                        
+                    if let dict = json as? [String: Any]{
+                        print("gogogo")
+                        print(dict["productList"] as! String)
+                        UserDefaults.standard.set(dict["productList"] as! String, forKey: "productList")
+                        UserDefaults.standard.set(true, forKey: "logged")
+                        }
+                    }
+                }
+            
+            }
+        else{
+            print(String(result))
+            }
+        }
+    }
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        if UserDefaults.standard.bool(forKey: "logged") {
+            self.presentController(withName: "GroceryController", context: nil)
+        }
         // Configure interface objects here.
     }
     
@@ -27,5 +80,42 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    func auth(email: String, password: String, with completion: @escaping (String) -> ()){
+            let url = URL(string: "https://recipe-finder-api-nodejs.herokuapp.com/login")!
+            print("yes1")
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            // Set HTTP Request Headers
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+            let newUser = [
+                "email": "\(email)",
+                "password": "\(password)"
+            ]
+                
+            let jsonData = try! JSONEncoder().encode(newUser)
+            request.httpBody = jsonData
+            print("yo")
+            URLSession.shared.dataTask(with: request){
+                data, response, error in
+                if let error = error {
+                    print("Error! \(error)")
+                }
+                if let data = data{
+                        print("go")
+                    if let result = String(data: data, encoding: .utf8){
+                        DispatchQueue.main.async {
+                            print("aiaia")
+                            completion(result)
+                            print("zoo")
+                        }
+                    }
+                }
+                    
+            }.resume()
+                
+        }
 
 }
+
