a26650666c3ab0db9e22c7d57920e743e85110a9
u.irene <janelake2017@gmail.com>
2020-08-13 18:22:12 +0300

    watch app button changes

--- RecipeFinder/RecipeFinder Watch Extension/InterfaceController.swift
+++ RecipeFinder/RecipeFinder Watch Extension/InterfaceController.swift
@@ -12,14 +12,23 @@ import CryptoKit
 import Alamofire
 import WatchConnectivity
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    @IBOutlet weak var notificationLabel: WKInterfaceLabel!
     @IBOutlet weak var GroceryTable: WKInterfaceTable!
     
     var colors: Dictionary = [0 : ""]
     var wcSession : WCSession!
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        
+        let groceryIngredients = UserDefaults.standard.stringArray(forKey: "ingredients") ?? []
+        loadGroceryTable(groceryIngredients: groceryIngredients, GroceryTable: GroceryTable)
+        if groceryIngredients != [] {
+            notificationLabel.setText("You need to buy " + String(groceryIngredients.count) + " products")
+            notificationLabel.setHidden(false)
+        }
+        else {
+            notificationLabel.setText("You have nothing to buy")
+            notificationLabel.setHidden(false)
+        }
         // Configure interface objects here.
     }
     
@@ -46,17 +55,18 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
                }
            }
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        
     }
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
         print("into")
         let groceryIngredients = message["message"] as! [String]
         print(groceryIngredients)
-//        var groceryIngredients : [String] = []
-//        for item in grocery {
-//            groceryIngredients.append(String(item))
-//        }
         print("GROCERY:", groceryIngredients)
+        loadGroceryTable(groceryIngredients: groceryIngredients, GroceryTable: GroceryTable)
+        notificationLabel.setText("You need to buy " + String(groceryIngredients.count) + " products")
+        notificationLabel.setHidden(false)
+        
+    }
+    func loadGroceryTable(groceryIngredients: [String], GroceryTable: WKInterfaceTable){
         var i : Int = 0
         GroceryTable.setNumberOfRows(groceryIngredients.count, withRowType: "Row")
         for item in groceryIngredients {
@@ -71,8 +81,8 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
             print("ROWS:", GroceryTable.numberOfRows)
             i += 1
         }
-           
-       }
+        UserDefaults.standard.set(groceryIngredients, forKey: "ingredients")
+    }
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
